d42d365a2404ed1a67d74aeb4db3d23a69e1b73f
CodeOne45 <malikaman1999@gmail.com>
2020-11-11 21:41:49 +0100

    Added header and footer

--- view/game.php
+++ view/game.php
@@ -1,1 +1,25 @@
 <?php
+include_once 'header.php';
+?>
+
+<?php
+include_once 'header.php';
+?>
+
+<body>
+    <form class="" action="loginphp" method="post">
+        <label for="">User name</label>
+        <input type="text" name="email" placeholder="Email" value="">
+
+        <label for="">Password</label>
+        <input type="password" name="password" placeholder="Password" value="">
+
+        <button type="submit" class="lbtn" name="log">Login</button>
+    </form>
+</body>
+
+</html>
+
+<?php
+include_once 'footer.php';
+?>
