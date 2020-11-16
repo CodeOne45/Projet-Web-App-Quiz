34f9a147722afe8abb0972a2cf21f3ff3e178a21
CodeOne45 <malikaman1999@gmail.com>
2020-11-16 21:09:50 +0100

    Added controller structure

---
+++ controllers/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace controller;
+
+/**
+ * Controller:
+ * 
+ * @author Aman,Flavio,Xiumin,Loqman & Steven
+ */
+class Controller
+{
+
+    protected $View = null;
+
+    public function __construct()
+    {
+        //Initialize a sesion
+        //Session::init();
+
+
+
+    }
+}
---
+++ controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace controller;
+
+use model;
+
+/**
+ * Index Controller :
+ * 
+ * @author Aman,Flavio,Xiumin,Loqman & Steven
+ */
