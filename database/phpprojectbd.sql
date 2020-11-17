3a1567aba098bf05d914312bf48eb93dd7c728f8
Xiumin Lin <xiumin.lin@etu.parisdescartes.fr>
2020-11-17 22:08:14 +0100

    Update database

--- database/phpprojectbd.sql
+++ database/phpprojectbd.sql
@@ -7,11 +7,9 @@
 -- Version du serveur :  10.4.14-MariaDB
 -- Version de PHP : 7.4.11
 
-SET SQL_MODE
-= "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone
-= "+00:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -29,14 +27,10 @@ SET time_zone
 -- Structure de la table `answer_participants`
 --
 
-CREATE TABLE `answer_participants`
-(
-  `id_player` int
-(10) NOT NULL,
-  `id_answer_question` int
-(10) NOT NULL,
-  `answer` char
-(100) DEFAULT NULL
+CREATE TABLE `answer_participants` (
+  `id_player` int(10) NOT NULL,
+  `id_answer_question` int(10) NOT NULL,
+  `answer` char(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -45,17 +39,12 @@ CREATE TABLE `answer_participants`
 -- Structure de la table `answer_question`
 --
 
-CREATE TABLE `answer_question`
-(
-  `id_answer_question` int
-(10) NOT NULL,
-  `id_question` int
-(10) NOT NULL,
+CREATE TABLE `answer_question` (
+  `id_answer_question` int(10) NOT NULL,
+  `id_question` int(10) NOT NULL,
   `answer` text DEFAULT NULL,
-  `correct` binary
-(1) DEFAULT NULL,
-  `point` int
-(2) NOT NULL
+  `correct` binary(1) DEFAULT NULL,
+  `point` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -64,12 +53,9 @@ CREATE TABLE `answer_question`
 -- Structure de la table `game`
 --
 
-CREATE TABLE `game`
-(
-  `id_game` int
-(10) NOT NULL,
-  `id_quiz` int
-(10) NOT NULL,
+CREATE TABLE `game` (
+  `id_game` int(10) NOT NULL,
+  `id_quiz` int(10) NOT NULL,
   `date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -79,18 +65,12 @@ CREATE TABLE `game`
 -- Structure de la table `player`
 --
 
-CREATE TABLE `player`
-(
-  `id_player` int
-(11) NOT NULL,
-  `id_game` int
-(10) NOT NULL,
-  `id_user` int
-(10) NOT NULL,
-  `Nickname` varchar
-(10) NOT NULL,
-  `score` int
-(3) NOT NULL
+CREATE TABLE `player` (
+  `id_player` int(11) NOT NULL,
+  `id_game` int(10) NOT NULL,
+  `id_user` int(10) NOT NULL,
+  `Nickname` varchar(10) NOT NULL,
+  `score` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -99,19 +79,13 @@ CREATE TABLE `player`
 -- Structure de la table `questions`
 --
 
-CREATE TABLE `questions`
-(
-  `id_questions` int
-(10) NOT NULL,
-  `id_quiz` int
-(10) NOT NULL,
-  `level` int
-(3) DEFAULT NULL,
-  `multiple_answer` binary
-(1) DEFAULT NULL,
+CREATE TABLE `questions` (
+  `id_questions` int(10) NOT NULL,
+  `id_quiz` int(10) NOT NULL,
+  `level` int(3) DEFAULT NULL,
+  `multiple_answer` binary(1) DEFAULT NULL,
   `question` text DEFAULT NULL,
-  `themes` varchar
-(20) NOT NULL
+  `themes` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -120,12 +94,9 @@ CREATE TABLE `questions`
 -- Structure de la table `quiz`
 --
 
-CREATE TABLE `quiz`
-(
-  `id_quiz` int
-(10) NOT NULL,
-  `name` char
-(20) DEFAULT NULL,
+CREATE TABLE `quiz` (
+  `id_quiz` int(10) NOT NULL,
+  `name` char(20) DEFAULT NULL,
   `description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -135,27 +106,12 @@ CREATE TABLE `quiz`
 -- Structure de la table `users`
 --
 
-CREATE TABLE `users`
-(
-  `id_user` int
-(10) NOT NULL,
-  `username` varchar
-(20) NOT NULL,
-  `email` varchar
-(50) NOT NULL,
-  `pwd` varchar
-(256) NOT NULL
+CREATE TABLE `users` (
+  `id_user` int(10) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `pwd` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`
-id_user`,
-`username
-`, `email`, `pwd`) VALUES
-(1, 'Aman', 'malikaman19@gmail.com', '$2y$10$/pIR4CgKImrLPCB3HEnlw.txDYV5zYnQn8rE96VeXg6GciRU9YNCG');
 
 --
 -- Index pour les tables déchargées
@@ -165,82 +121,59 @@ id_user`,
 -- Index pour la table `answer_participants`
 --
 ALTER TABLE `answer_participants`
-ADD KEY `FK_id_player`
-(`id_player`),
-ADD KEY `FK_id_answer_question`
-(`id_answer_question`);
+  ADD KEY `FK_id_player` (`id_player`),
+  ADD KEY `FK_id_answer_question` (`id_answer_question`);
 
 --
 -- Index pour la table `answer_question`
 --
 ALTER TABLE `answer_question`
-ADD PRIMARY KEY
-(`id_answer_question`),
-ADD UNIQUE KEY `id_answer_question`
-(`id_answer_question`),
-ADD KEY `FK_id_question`
-(`id_question`);
+  ADD PRIMARY KEY (`id_answer_question`),
+  ADD UNIQUE KEY `id_answer_question` (`id_answer_question`),
+  ADD KEY `FK_id_question` (`id_question`);
 
 --
 -- Index pour la table `game`
 --
 ALTER TABLE `game`
-ADD PRIMARY KEY
-(`id_game`),
-ADD UNIQUE KEY `id_game`
-(`id_game`),
-ADD KEY `FK_id_quiz`
-(`id_quiz`);
+  ADD PRIMARY KEY (`id_game`),
+  ADD UNIQUE KEY `id_game` (`id_game`),
+  ADD KEY `FK_id_quiz` (`id_quiz`);
 
 --
 -- Index pour la table `player`
 --
 ALTER TABLE `player`
-ADD PRIMARY KEY
-(`id_player`),
-ADD UNIQUE KEY `id_player`
-(`id_player`),
-ADD UNIQUE KEY `Nickname`
-(`Nickname`),
-ADD KEY `FK_id_user`
-(`id_user`),
-ADD KEY `FK_id_game`
-(`id_game`);
+  ADD PRIMARY KEY (`id_player`),
+  ADD UNIQUE KEY `id_player` (`id_player`),
+  ADD UNIQUE KEY `Nickname` (`Nickname`),
+  ADD KEY `FK_id_user` (`id_user`),
+  ADD KEY `FK_id_game` (`id_game`);
 
 --
 -- Index pour la table `questions`
 --
 ALTER TABLE `questions`
-ADD PRIMARY KEY
-(`id_questions`),
-ADD UNIQUE KEY `id_questions`
-(`id_questions`),
-ADD KEY `FK_quiz_id`
-(`id_quiz`);
+  ADD PRIMARY KEY (`id_questions`),
+  ADD UNIQUE KEY `id_questions` (`id_questions`),
+  ADD KEY `FK_quiz_id` (`id_quiz`);
 
 --
 -- Index pour la table `quiz`
 --
 ALTER TABLE `quiz`
-ADD PRIMARY KEY
-(`id_quiz`),
-ADD UNIQUE KEY `id_quiz`
-(`id_quiz`),
-ADD UNIQUE KEY `name`
-(`name`);
+  ADD PRIMARY KEY (`id_quiz`),
+  ADD UNIQUE KEY `id_quiz` (`id_quiz`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
-ADD PRIMARY KEY
-(`id_user`),
-ADD UNIQUE KEY `id_user`
-(`id_user`),
-ADD UNIQUE KEY `username`
-(`username`),
-ADD UNIQUE KEY `email`
-(`email`);
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `id_user` (`id_user`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -250,36 +183,31 @@ ADD UNIQUE KEY `email`
 -- AUTO_INCREMENT pour la table `answer_question`
 --
 ALTER TABLE `answer_question`
-  MODIFY `id_answer_question` int
-(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_answer_question` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `player`
 --
 ALTER TABLE `player`
-  MODIFY `id_player` int
-(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_player` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id_questions` int
-(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_questions` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `quiz`
 --
 ALTER TABLE `quiz`
-  MODIFY `id_quiz` int
-(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_quiz` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int
-(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- Contraintes pour les tables déchargées
@@ -289,61 +217,33 @@ ALTER TABLE `users`
 -- Contraintes pour la table `answer_participants`
 --
 ALTER TABLE `answer_participants`
-ADD CONSTRAINT `FK_id_answer_question` FOREIGN KEY
-(`id_answer_question`) REFERENCES `answer_question`
-(`id_answer_question`) ON
-DELETE CASCADE ON
-UPDATE CASCADE,
-ADD CONSTRAINT `FK_id_player` FOREIGN KEY
-(`id_player`) REFERENCES `player`
-(`id_player`) ON
-DELETE CASCADE ON
-UPDATE CASCADE;
+  ADD CONSTRAINT `FK_id_answer_question` FOREIGN KEY (`id_answer_question`) REFERENCES `answer_question` (`id_answer_question`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_id_player` FOREIGN KEY (`id_player`) REFERENCES `player` (`id_player`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `answer_question`
 --
 ALTER TABLE `answer_question`
-ADD CONSTRAINT `FK_id_question` FOREIGN KEY
-(`id_question`) REFERENCES `answer_question`
-(`id_answer_question`) ON
-DELETE CASCADE ON
-UPDATE CASCADE;
+  ADD CONSTRAINT `FK_id_question` FOREIGN KEY (`id_question`) REFERENCES `answer_question` (`id_answer_question`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `game`
 --
 ALTER TABLE `game`
-ADD CONSTRAINT `FK_id_quiz` FOREIGN KEY
-(`id_quiz`) REFERENCES `quiz`
-(`id_quiz`) ON
-DELETE CASCADE ON
-UPDATE CASCADE;
+  ADD CONSTRAINT `FK_id_quiz` FOREIGN KEY (`id_quiz`) REFERENCES `quiz` (`id_quiz`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `player`
 --
 ALTER TABLE `player`
-ADD CONSTRAINT `FK_id_game` FOREIGN KEY
-(`id_game`) REFERENCES `game`
-(`id_game`) ON
-DELETE CASCADE ON
-UPDATE CASCADE,
-ADD CONSTRAINT `FK_id_user` FOREIGN KEY
-(`id_user`) REFERENCES `users`
-(`id_user`) ON
-DELETE CASCADE ON
-UPDATE CASCADE;
+  ADD CONSTRAINT `FK_id_game` FOREIGN KEY (`id_game`) REFERENCES `game` (`id_game`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_id_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `questions`
 --
 ALTER TABLE `questions`
-ADD CONSTRAINT `FK_quiz_id` FOREIGN KEY
-(`id_quiz`) REFERENCES `quiz`
-(`id_quiz`) ON
-DELETE CASCADE ON
-UPDATE CASCADE;
+  ADD CONSTRAINT `FK_quiz_id` FOREIGN KEY (`id_quiz`) REFERENCES `quiz` (`id_quiz`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
