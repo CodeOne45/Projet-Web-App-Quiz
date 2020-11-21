bfec2a6b385b7471e656da5bd10c9f8c7402846a
Steven <steven.van@etu.parisdescartes.fr>
2020-11-21 11:57:57 +0100

    add routes and composer

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require '../vendor/autoload.php';
+$uri = $_SERVER['REQUEST_URI'];
+$router = new AltoRouter();
+
+$router->map('GET', '/', '../views/index');
+
+$match = $router->match();
+
+require '../views/header.php';
+if (is_array($match)) {
+    $params = $match['params'];
+    require "../views/{$match['target']}.php";
+    } 
+    else {
+    echo 404;
+    }
+require '../views/footer.php';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c55317a7d818c0cc5c71e86c833adb61e3049565',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c55317a7d818c0cc5c71e86c833adb61e3049565',
+    ),
+    'altorouter/altorouter' => 
+    array (
+      'pretty_version' => '2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '127f6e96998708a31ef32252985bea82e3b03888',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '39d483bdf39be819deabf04ec872eb0b2410b531',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e70aa8b064c5b72d3df2abd5ab1e90464ad009de',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => 'v5.1.8',
+      'version' => '5.1.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4e13f3fcefb1fcaaa5efb5403581406f4e840b9a',
+    ),
+  ),
+);
